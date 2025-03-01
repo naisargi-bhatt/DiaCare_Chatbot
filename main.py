eadb399a3d879e1b6e8d07dafad8cf2de8a22205
Tanvi <tanvibalsara18@gmail.com>
2025-03-01 10:38:06 +0530

    Update main file

--- main.py
+++ main.py
@@ -168,6 +168,7 @@ def get_gemini_response(question, max_words=60):
 @app.get("/")
 def read_root():
     return {"message": "FastAPI is running!"}
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Use Render's PORT or default to 8000
     print(f"Starting server on port {port}...")  # Debugging log
